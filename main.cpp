3d043944606521e3e8ffdf416139afda2029beb0
junios <junios@naver.com>
2023-11-13 14:45:37 +0900

    STL Iterator

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
 
 #include "Player.h"
 #include "Goblin.h"
@@ -9,30 +12,49 @@ using namespace std;
 
 int main()
 {
-	FPlayer* MyPlayer = new FPlayer();
+	//STL, Container
+	vector<int> IntArray;
+	IntArray.push_back(1);
+	IntArray.push_back(2);
+	IntArray.push_back(3);
 
-	MyPlayer->Move();
+	IntArray.erase(find(IntArray.begin(), IntArray.end(), 2));
 
-	delete MyPlayer;
+	auto FindValue = find(IntArray.begin(), IntArray.end(), 2);
+	
 
-	FSlime* YourSlime = new FSlime();
+	//for (auto i = IntArray.rbegin(); i != IntArray.rend(); ++i)
+	for (auto Value : IntArray)
+	{
+	}
 
-	YourSlime->Move();
+	list<int> IntList;
+	IntList.push_back(1);
+	IntList.push_back(2);
+	IntList.push_back(3);
+	for (auto Value : IntList)
+	{
+		cout << Value << endl;
+	}
+	for (list<int>::reverse_iterator i = IntList.rbegin(); i != IntList.rend(); ++i)
+	{
+		cout << *i << endl;
+	}
 
-	delete YourSlime;
-
-	FCharacter* MyCharacter = new FCharacter();
-
-	MyCharacter->Move();
-
-	delete MyCharacter;
 
 
-	FGoblin* YourGoblin = new FGoblin();
 
-	YourGoblin->Move();
+	//vector<FPlayer*> Player;
+	//Player.push_back(new FPlayer());
 
-	delete YourGoblin;
+	//vector<FSlime*> Slimes;
+	//Slimes.push_back(new FSlime());
+
+	//vector<FBoar*> Boars;
+	//Boars.push_back(new FBoar());
+
+	//vector<FGoblin*> Goblins;
+	//Goblins.push_back(new FGoblin());
 
 	return 0;
 }
